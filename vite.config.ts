69db35ebf187a64b14f2e28091e4e84576f66104
Suyash Jadhav <Suyashjadhav710@gmail.com>
2026-08-09 16:27:05 +0530

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-  base: "/ddomics-lab-framework/",
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
-});
