6643166ed202fa50682a6c5b265e98bc700eb9b1
Suyash Jadhav <Suyashjadhav710@gmail.com>
2026-09-11 09:03:03 +0530

    Remove unnecessary gradient divs from DomainsMarquee
    Removed gradient divs that were not needed.

--- src/components/DomainsMarquee.tsx
+++ src/components/DomainsMarquee.tsx
@@ -33,8 +33,6 @@ export function DomainsMarquee() {
           </Link>
         ))}
       </div>
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-16 bg-gradient-to-r from-surface to-transparent sm:w-24" />
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-16 bg-gradient-to-l from-surface to-transparent sm:w-24" />
     </div>
   );
 }
